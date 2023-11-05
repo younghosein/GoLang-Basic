40865662109f708d277a5c0b515c818e7f16a7d4
Hosein Mohamadpor <83702864+younghosein@users.noreply.github.com>
2023-11-05 15:48:25 +0330

    Add files via upload

---
+++ main/constant.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+const pi float32 = 3.14
+
+const (
+	a int = 8
+	b     = 3.14
+	c     = "hi"
+)
+
+func main() {
+	fmt.Println(pi)
+
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+
+}
